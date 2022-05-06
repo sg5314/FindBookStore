aca017217bb41b714e16b69c26301a7f9b5042b2
Sugahara Shigetaka <shigetaka@SugaharanoMacBook-Air.local>
2022-05-06 23:16:02 +0900

    [add] add unit test

--- FindBookStoreApp/tests.py
+++ FindBookStoreApp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import searchfunc, storefunc, kinokuniya_store_func, searchBook
+import unittest
+
+
 
 # Create your tests here.
+
+
+class TestUrls(TestCase):
+    def test_post_index_url(self):
+        resolve('/')
+        resolve('/stores/123456')# 適当な数字
+        resolve('/kinokuniya/123456')
+
+
+    @unittest.skip('Google_books_apiのテスト')
+    def check_isbn_api(self):
+        self.assertTrue(searchBook('Django'))
+
+    
