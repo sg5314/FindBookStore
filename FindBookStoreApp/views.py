db800dedf398e87194063b3b42225b89cb76a47b
Sugahara Shigetaka <shigetaka@SugaharanoMacBook-Air.local>
2022-05-06 23:21:45 +0900

    [fix] Improved readability

--- FindBookStoreApp/views.py
+++ FindBookStoreApp/views.py
@@ -13,16 +13,6 @@ def searchfunc(request):
     if request.method == 'POST':
         title = request.POST['title']
         BooksData = searchBook(title)
-
-        #print(BooksData['items'])
-
-        """Booksdf = pd.DataFrame({
-            'items': BooksData['items'],
-            'isbn' : BooksData['industryIdentifiers']
-        })
-
-        print(Booksdf)"""
-
         
         books_data = {}
         id = 0
@@ -40,9 +30,6 @@ def searchfunc(request):
                     isbn = 'null'
 
                 if len(con['volumeInfo']['industryIdentifiers']) == 2:
-                    """isbn = {con['volumeInfo']['industryIdentifiers'][0]['type']: con['volumeInfo']['industryIdentifiers'][0]['identifier'],
-                            con['volumeInfo']['industryIdentifiers'][1]['type']: con['volumeInfo']['industryIdentifiers'][1]['identifier']}
-                    """  
                     # ISBN_13 を取得
                     if con['volumeInfo']['industryIdentifiers'][0]['type'] == "ISBN_13":
                         isbn = int(con['volumeInfo']['industryIdentifiers'][0]['identifier'])
@@ -69,43 +56,10 @@ def searchfunc(request):
             id += 1
             print('-----------------------------------\n')
 
-        
-        #print(json_str)
-        #return HttpResponse(BooksData)
         return render(request, 'index.html', {'books_data':books_data, 'query':title})
     else:
         # https://api.openbd.jp/v1/get?isbn={ISBN}
         return render(request, 'index.html')
-    return HttpResponse('output')
-
-"""def storefunc(request):
-
-    keyword_url = 'https://honto.jp/netstore/pd-store_0630360105.html'
-    isbn_10 = '4798062219'
-    isbn_13 = '9784798062211'
-
-    #keyword_url = "http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=2088108&amp;pid=884062438&amp;vc_url=http%3a%2f%2fhonto.jp%2fisbn%2f" + str(isbn)
-    keyword_url = 'https://honto.jp/netstore/pd-store_0630360105.html'
-
-
-
-    book_stores = {'kinokuniya': 'https://www.kinokuniya.co.jp/f/dsg-01-' + isbn_13,
-                    'amazon': 'https://www.amazon.co.jp/exec/obidos/ASIN/'+ isbn_10 +'/honnoinfo-22/',
-                    'yodobasi': 'https://www.yodobashi.com/?word=' + isbn_13
-    }
-
-    r = requests.get(keyword_url)
-    print(r.text)
-    
-    soup = BeautifulSoup(r.content, "lxml")
-
-    p = soup.find('script', {'type':'application/ld+json'})
-    print (p)
-    
-    #response = json.dumps(requests.get(keyword_url).json(), indent=2)
-    #remindId
-
-    return render(request, 'store.html', {'book_stores':book_stores})"""
 
 def jan_to_asin(jan13):
     s = str(jan13)[3:12]
@@ -167,24 +121,7 @@ def kinokuniya_store_func(request, pk):
                 "region" : i['address']['region'],
                 "store_name" : i['title']
             }]
-
-
-        """kinokuniya_stores[count] = {
-            'store_id' : i['id'],
-            'stock_url' : 'https://www.kinokuniya.co.jp/kinonavi/disp/CKnNvSfGoodsPage.jsp' + i['service_urls'][1]['url'].split('/')[-1] + '&ptk=01&CAT=01&GOODS_STK_NO=' + str(pk),
-            'region' : i['address']['region'],
-            'store_name' : i['title']
-        }"""
         count += 1
-        """print('-----------------------------')
-        pprint.pprint(i['title'])
-        pprint.pprint(i['service_urls'][1]['url'])
-        pprint.pprint(i['address']['region'])
-        pprint.pprint(i['id'])
-        print('-----------------------------')"""
-
-        #pprint.pprint(kinokuniya_stores)
-        #print(",".join(list(kinokuniya_stores.keys())))
         print('-------------------------')
 
     return render(request, 'kinokuniya.html', {'kinokuniya_stores' : kinokuniya_stores })
